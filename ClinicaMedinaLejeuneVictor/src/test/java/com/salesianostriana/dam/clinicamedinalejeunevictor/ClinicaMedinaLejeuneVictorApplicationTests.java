12ec91af41f7ad9b0b0e592d3cf3b260e9d72ae0
Victor <medina.levic24@triana.salesianos.edu>
2024-05-06 12:27:06 +0200

    Creacion del proyecto

---
+++ ClinicaMedinaLejeuneVictor/src/test/java/com/salesianostriana/dam/clinicamedinalejeunevictor/ClinicaMedinaLejeuneVictorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.clinicamedinalejeunevictor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicaMedinaLejeuneVictorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

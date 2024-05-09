8173a81f56809d2b9cf5557c366bc5ae1ff8c609
Victor <medina.levic24@triana.salesianos.edu>
2024-05-09 08:32:42 +0200

    Clases clientes y seguro con asociaciones

--- ClinicaMedinaLejeuneVictor/src/main/java/com/salesianostriana/dam/clinicamedinalejeunevictor/modelos/Cliente.java
+++ ClinicaMedinaLejeuneVictor/src/main/java/com/salesianostriana/dam/clinicamedinalejeunevictor/modelos/Cliente.java
@@ -1,6 +1,10 @@
 package com.salesianostriana.dam.clinicamedinalejeunevictor.modelos;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.ForeignKey;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,11 +13,25 @@ import lombok.ToString;
 
 @Entity
 @NoArgsConstructor
+@AllArgsConstructor
 @Getter
 @Setter
 @ToString(callSuper = true)
 @EqualsAndHashCode(callSuper = true)
 public class Cliente extends Usuario {
 
-	private int tipoSeguro;
+	@ManyToOne
+	@JoinColumn(foreignKey = @ForeignKey(name = "fk_cliente_seguro"))
+	private Seguro seguro;
+
+	public void addToSeguro(Seguro seguro) {
+		this.seguro = seguro;
+		seguro.getClientes().add(this);
+	}
+
+	public void removeFromSeguro(Seguro seguro) {
+		seguro.getClientes().remove(this);
+		this.seguro = null;
+	}
+
 }

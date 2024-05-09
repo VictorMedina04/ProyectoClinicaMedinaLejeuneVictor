bb0e6a417cace0c407067e738dbae3852501b6b3
Victor <medina.levic24@triana.salesianos.edu>
2024-05-09 13:18:19 +0200

    seguro y departamento servicio

---
+++ ClinicaMedinaLejeuneVictor/src/main/java/com/salesianostriana/dam/clinicamedinalejeunevictor/servicios/DepartamentoServicio.java
@@ -0,0 +1,9 @@
+package com.salesianostriana.dam.clinicamedinalejeunevictor.servicios;
+
+import com.salesianostriana.dam.clinicamedinalejeunevictor.modelos.Departamento;
+import com.salesianostriana.dam.clinicamedinalejeunevictor.repositorios.DepartamentoRepositorios;
+import com.salesianostriana.dam.clinicamedinalejeunevictor.servicios.serviciosbase.BaseServiceImpl;
+
+public class DepartamentoServicio extends BaseServiceImpl<Departamento, Long, DepartamentoRepositorios> {
+
+}
---
+++ ClinicaMedinaLejeuneVictor/src/main/java/com/salesianostriana/dam/clinicamedinalejeunevictor/servicios/SeguroServicio.java
@@ -0,0 +1,9 @@
+package com.salesianostriana.dam.clinicamedinalejeunevictor.servicios;
+
+import com.salesianostriana.dam.clinicamedinalejeunevictor.modelos.Seguro;
+import com.salesianostriana.dam.clinicamedinalejeunevictor.repositorios.SeguroRepositorio;
+import com.salesianostriana.dam.clinicamedinalejeunevictor.servicios.serviciosbase.BaseServiceImpl;
+
+public class SeguroServicio extends BaseServiceImpl<Seguro, Long, SeguroRepositorio> {
+
+}

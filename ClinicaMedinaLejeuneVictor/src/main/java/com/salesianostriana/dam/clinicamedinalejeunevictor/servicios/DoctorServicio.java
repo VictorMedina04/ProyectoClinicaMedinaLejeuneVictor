034acbfdfcf900e15c6916f267512b6120fd533c
Victor <medina.levic24@triana.salesianos.edu>
2024-05-10 11:34:52 +0200

    Cambios en doctor

--- ClinicaMedinaLejeuneVictor/src/main/java/com/salesianostriana/dam/clinicamedinalejeunevictor/servicios/DoctorServicio.java
+++ ClinicaMedinaLejeuneVictor/src/main/java/com/salesianostriana/dam/clinicamedinalejeunevictor/servicios/DoctorServicio.java
@@ -3,11 +3,10 @@ package com.salesianostriana.dam.clinicamedinalejeunevictor.servicios;
 import org.springframework.stereotype.Service;
 
 import com.salesianostriana.dam.clinicamedinalejeunevictor.modelos.Doctor;
-import com.salesianostriana.dam.clinicamedinalejeunevictor.modelos.DoctorPk;
 import com.salesianostriana.dam.clinicamedinalejeunevictor.repositorios.DoctorRepositorio;
 import com.salesianostriana.dam.clinicamedinalejeunevictor.servicios.serviciosbase.BaseServiceImpl;
 
 @Service
-public class DoctorServicio extends BaseServiceImpl<Doctor, DoctorPk, DoctorRepositorio> {
+public class DoctorServicio extends BaseServiceImpl<Doctor, Long, DoctorRepositorio> {
 
 }

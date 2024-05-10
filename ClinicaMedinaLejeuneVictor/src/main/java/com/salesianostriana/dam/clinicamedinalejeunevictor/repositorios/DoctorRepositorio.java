f50b9d0d96054e9b8c690bbeb49e801570447daf
Victor <medina.levic24@triana.salesianos.edu>
2024-05-10 11:34:09 +0200

    Cambios de doctor

--- ClinicaMedinaLejeuneVictor/src/main/java/com/salesianostriana/dam/clinicamedinalejeunevictor/repositorios/DoctorRepositorio.java
+++ ClinicaMedinaLejeuneVictor/src/main/java/com/salesianostriana/dam/clinicamedinalejeunevictor/repositorios/DoctorRepositorio.java
@@ -3,8 +3,7 @@ package com.salesianostriana.dam.clinicamedinalejeunevictor.repositorios;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.salesianostriana.dam.clinicamedinalejeunevictor.modelos.Doctor;
-import com.salesianostriana.dam.clinicamedinalejeunevictor.modelos.DoctorPk;
 
-public interface DoctorRepositorio extends JpaRepository<Doctor, DoctorPk> {
+public interface DoctorRepositorio extends JpaRepository<Doctor, Long> {
 
 }

bb19f9a3ba07d3d20a28898b32a87101b9ec9b7a
Victor <medina.levic24@triana.salesianos.edu>
2024-05-15 13:29:42 +0200

    servicios hechos

--- ClinicaMedinaLejeuneVictor/src/main/java/com/salesianostriana/dam/clinicamedinalejeunevictor/servicios/UserDetailsServiceImpl.java
+++ ClinicaMedinaLejeuneVictor/src/main/java/com/salesianostriana/dam/clinicamedinalejeunevictor/servicios/UserDetailsServiceImpl.java
@@ -1,20 +1,23 @@
-//package com.salesianostriana.dam.clinicamedinalejeunevictor.servicios;
-//
-//import org.springframework.stereotype.Service;
-//
-//import com.salesianostriana.dam.clinicamedinalejeunevictor.repositorios.UsuarioRepositorio;
-//
-//import lombok.RequiredArgsConstructor;
-//
-//@Service
-//@RequiredArgsConstructor
-//public class UserDetailsServiceImpl implements UserDe {
-//
-//	private final UsuarioRepositorio repo;
-//
-//	@Override
-//	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-//		return repo.findFirstByUsername(username)
-//				.orElseThrow(() -> new UsernameNotFoundException("Error al buscar el usuario"));
-//	}
-//}
+package com.salesianostriana.dam.clinicamedinalejeunevictor.servicios;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.salesianostriana.dam.clinicamedinalejeunevictor.repositorios.UsuarioRepositorio;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+	private final UsuarioRepositorio repo;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		return repo.findFirstByUsername(username)
+				.orElseThrow(() -> new UsernameNotFoundException("Error al buscar el usuario"));
+	}
+}

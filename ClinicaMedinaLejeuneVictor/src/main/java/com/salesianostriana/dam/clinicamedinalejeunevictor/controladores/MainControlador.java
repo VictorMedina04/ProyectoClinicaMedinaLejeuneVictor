4d9a18196029794449b52479bb608b8ba03756bd
Victor <medina.levic24@triana.salesianos.edu>
2024-05-16 09:05:19 +0200

    controlador e import

--- ClinicaMedinaLejeuneVictor/src/main/java/com/salesianostriana/dam/clinicamedinalejeunevictor/controladores/MainControlador.java
+++ ClinicaMedinaLejeuneVictor/src/main/java/com/salesianostriana/dam/clinicamedinalejeunevictor/controladores/MainControlador.java
@@ -1,10 +1,17 @@
 package com.salesianostriana.dam.clinicamedinalejeunevictor.controladores;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.salesianostriana.dam.clinicamedinalejeunevictor.servicios.DoctorServicio;
 
 @Controller
 public class MainControlador {
+
+	@Autowired
+	private DoctorServicio doctorServicio;
 
 	@GetMapping("/")
 	public String inicioSinLogin() {
@@ -14,5 +21,11 @@ public class MainControlador {
 	@GetMapping("/login")
 	public String formLogin() {
 		return "login";
+	}
+
+	@GetMapping("/doctores")
+	public String doctoresSinLog(Model model) {
+		model.addAttribute("doctores", doctorServicio.findAll());
+		return "doctores";
 	}
 }
--- ClinicaMedinaLejeuneVictor/src/main/resources/import.sql
+++ ClinicaMedinaLejeuneVictor/src/main/resources/import.sql
@@ -16,11 +16,17 @@ INSERT INTO seguro(ID_SEGURO, NOMBRE, PRECIO, DESCRIPCION) VALUES(3, 'Seguro oro
 
 INSERT INTO usuario(ID, USERNAME, NOMBRE, APELLIDOS, GMAIL, PASSWORD, DNI, ES_ADMIN, ES_DOCTOR) VALUES(1,'admin','Paco','Gomez','pacogomez@gmail.com','{bcrypt}$2y$10$gk.yHcB4aPtgs6zNYV6ve.4cgKMozKboCnIVj7a01ckM7sae9nXkW','12345678A',true,false);
 
-INSERT INTO usuario(ID, USERNAME, NOMBRE, APELLIDOS, GMAIL, PASSWORD, DNI, ES_ADMIN, ES_DOCTOR) VALUES(2,'Doctor01','Jose','Garcia','josegarcia@gmail.com','{bcrypt}$2y$10$gk.yHcB4aPtgs6zNYV6ve.4cgKMozKboCnIVj7a01ckM7sae9nXkW','23456789B',false,true);
+INSERT INTO usuario(ID, username, NOMBRE, APELLIDOS, GMAIL, password, DNI, ES_ADMIN, ES_DOCTOR) VALUES(2,'Doctor01','Jose','Garcia','josegarcia@gmail.com','{bcrypt}$2y$10$o.EOfX3DcTdr9ku1PpTmX.4pnJ/Tpgy.WHtB/Z00ElYaD1VfsnEJO','23456789B',false,true);
 
-INSERT INTO usuario(ID, USERNAME, NOMBRE, APELLIDOS, GMAIL, PASSWORD, DNI, ES_ADMIN, ES_DOCTOR) VALUES(5,'Doctor02','Marta','Lopez','martalopez@gmail.com','{bcrypt}$2y$10$gk.yHcB4aPtgs6zNYV6ve.4cgKMozKboCnIVj7a01ckM7sae9nXkW','56789123E',false,true);
+INSERT INTO usuario(ID, username, NOMBRE, APELLIDOS, GMAIL, password, DNI, ES_ADMIN, ES_DOCTOR) VALUES(5,'Doctor02','Marta','Lopez','martalopez@gmail.com','{bcrypt}$2y$10$o.EOfX3DcTdr9ku1PpTmX.4pnJ/Tpgy.WHtB/Z00ElYaD1VfsnEJO','34567891C',false,true);
 
-INSERT INTO usuario(ID, USERNAME, NOMBRE, APELLIDOS, GMAIL, PASSWORD, DNI, ES_ADMIN, ES_DOCTOR) VALUES(6,'Doctor03','Angela','Martín','angelamartin@gmail.com','{bcrypt}$2y$10$gk.yHcB4aPtgs6zNYV6ve.4cgKMozKboCnIVj7a01ckM7sae9nXkW','67891234F',false,true);
+INSERT INTO usuario(ID, username, NOMBRE, APELLIDOS, GMAIL, password, DNI, ES_ADMIN, ES_DOCTOR) VALUES(6,'Doctor03','Angela','Martín','angelamartin@gmail.com','{bcrypt}$2y$10$o.EOfX3DcTdr9ku1PpTmX.4pnJ/Tpgy.WHtB/Z00ElYaD1VfsnEJO','45678912D',false,true);
+
+INSERT INTO usuario(ID, username, NOMBRE, APELLIDOS, GMAIL, password, DNI, ES_ADMIN, ES_DOCTOR) VALUES(7, 'Doctor04', 'Juan', 'Lopez', 'juanlopez@gmail.com', '{bcrypt}$2y$10$o.EOfX3DcTdr9ku1PpTmX.4pnJ/Tpgy.WHtB/Z00ElYaD1VfsnEJO', '56789123E', false, true);
+
+INSERT INTO usuario(ID, username, NOMBRE, APELLIDOS, GMAIL, password, DNI, ES_ADMIN, ES_DOCTOR) VALUES(8, 'Doctor05', 'María', 'Martínez', 'mariamartinez@gmail.com', '{bcrypt}$2y$10$o.EOfX3DcTdr9ku1PpTmX.4pnJ/Tpgy.WHtB/Z00ElYaD1VfsnEJO', '67891234F', false, true);
+
+INSERT INTO usuario(ID, username, NOMBRE, APELLIDOS, GMAIL, password, DNI, ES_ADMIN, ES_DOCTOR) VALUES(9, 'Doctor06', 'Ana', 'Sánchez', 'anasanchez@gmail.com', '{bcrypt}$2y$10$o.EOfX3DcTdr9ku1PpTmX.4pnJ/Tpgy.WHtB/Z00ElYaD1VfsnEJO', '78912345G', false, true);
 
 INSERT INTO usuario(ID, USERNAME, NOMBRE, APELLIDOS, GMAIL, PASSWORD, DNI, ES_ADMIN, ES_DOCTOR) VALUES(3,'Cliente01','Maria','Medina','mariamedina@gmail.com','{bcrypt}$2y$10$gk.yHcB4aPtgs6zNYV6ve.4cgKMozKboCnIVj7a01ckM7sae9nXkW','34567891C',false,false);
 
@@ -32,6 +38,12 @@ INSERT INTO doctor(ID,DEPARTAMENTO_ID_DEPARTAMENTO,SALARIO,IND_JEFE) VALUES(2,1,
 INSERT INTO doctor(ID,DEPARTAMENTO_ID_DEPARTAMENTO,SALARIO,IND_JEFE) VALUES(5,2,250,true);
 
 INSERT INTO doctor(ID,DEPARTAMENTO_ID_DEPARTAMENTO,SALARIO,IND_JEFE) VALUES(6,3,170,false);
+
+INSERT INTO doctor(ID,DEPARTAMENTO_ID_DEPARTAMENTO,SALARIO,IND_JEFE) VALUES(7,1,170,true);
+
+INSERT INTO doctor(ID,DEPARTAMENTO_ID_DEPARTAMENTO,SALARIO,IND_JEFE) VALUES(8,2,170,false);
+
+INSERT INTO doctor(ID,DEPARTAMENTO_ID_DEPARTAMENTO,SALARIO,IND_JEFE) VALUES(9,3,170,true);
   	  	  	  
 
 

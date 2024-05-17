16ace312ec45816472b2ccef1a26ae52337ef4b6
Victor <medina.levic24@triana.salesianos.edu>
2024-05-17 12:59:48 +0200

    controlador para editar, cambio en el import y cambios para editar en el formulario y tabla

--- ClinicaMedinaLejeuneVictor/src/main/java/com/salesianostriana/dam/clinicamedinalejeunevictor/controladores/AdminControlador.java
+++ ClinicaMedinaLejeuneVictor/src/main/java/com/salesianostriana/dam/clinicamedinalejeunevictor/controladores/AdminControlador.java
@@ -1,12 +1,14 @@
 package com.salesianostriana.dam.clinicamedinalejeunevictor.controladores;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -48,6 +50,26 @@ public class AdminControlador {
 
 		doctorServicio.save(doctor);
 
+		return "redirect:/admin/mostrarDoctores";
+	}
+
+	// formulario para editar doctor
+	@GetMapping("/editarDoctor/{id}")
+	public String mostrarFormularioEdicion(@PathVariable("id") Long id, Model model) {
+
+		Optional<Doctor> editarDoctor = doctorServicio.findById(id);
+		List<Departamento> departamentos = departamentoServicio.findAll();
+
+		model.addAttribute("departamentos", departamentos);
+		model.addAttribute("doctor", editarDoctor.get());
+
+		return "admin/formularioDoctor";
+	}
+
+	// confirmar edicion doctor
+	@PostMapping("/editarDoctor/submit")
+	public String procesarFormularioEdicion(@ModelAttribute("doctor") Doctor doctor) {
+		doctorServicio.edit(doctor);
 		return "redirect:/admin/mostrarDoctores";
 	}
 

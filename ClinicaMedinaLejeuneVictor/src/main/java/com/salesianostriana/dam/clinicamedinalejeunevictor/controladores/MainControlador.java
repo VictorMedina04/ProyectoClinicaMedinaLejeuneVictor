7fdc724560430193024913fb6440334bace0be20
Victor <medina.levic24@triana.salesianos.edu>
2024-06-11 11:46:46 +0200

    formulario de registro validado

--- ClinicaMedinaLejeuneVictor/src/main/java/com/salesianostriana/dam/clinicamedinalejeunevictor/controladores/MainControlador.java
+++ ClinicaMedinaLejeuneVictor/src/main/java/com/salesianostriana/dam/clinicamedinalejeunevictor/controladores/MainControlador.java
@@ -1,5 +1,7 @@
 package com.salesianostriana.dam.clinicamedinalejeunevictor.controladores;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,8 +10,12 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.salesianostriana.dam.clinicamedinalejeunevictor.modelos.Cliente;
+import com.salesianostriana.dam.clinicamedinalejeunevictor.modelos.Seguro;
+import com.salesianostriana.dam.clinicamedinalejeunevictor.modelos.Usuario;
 import com.salesianostriana.dam.clinicamedinalejeunevictor.servicios.ClienteServicio;
 import com.salesianostriana.dam.clinicamedinalejeunevictor.servicios.DoctorServicio;
+import com.salesianostriana.dam.clinicamedinalejeunevictor.servicios.SeguroServicio;
+import com.salesianostriana.dam.clinicamedinalejeunevictor.servicios.UsuarioServicio;
 
 @Controller
 public class MainControlador {
@@ -19,6 +25,12 @@ public class MainControlador {
 
 	@Autowired
 	private ClienteServicio clienteServicio;
+
+	@Autowired
+	private SeguroServicio seguroServicio;
+
+	@Autowired
+	private UsuarioServicio usuarioServicio;
 
 	@GetMapping("/")
 	public String inicioSinLogin() {
@@ -36,16 +48,24 @@ public class MainControlador {
 		return "doctores";
 	}
 
-	// se registran todos los que quieran pero como clientes
 	@GetMapping("/nuevoCliente")
 	public String mostrarFormulario(Model model) {
+		List<Seguro> seguros = seguroServicio.findAll();
+		model.addAttribute("seguros", seguros);
 		model.addAttribute("clienteForm", new Cliente());
 		return "registroCliente";
 	}
 
 	@PostMapping("/nuevoCliente/submit")
-	public String procesarFormulario(@ModelAttribute("clienteForm") Cliente cliente) {
-		clienteServicio.save(cliente);
+	public String procesarFormulario(@ModelAttribute("clienteForm") Usuario usuario) {
+
+		if (usuarioServicio.encontrarPorUsername(usuario.getUsername())) {
+			return "errorUsername";
+		}
+
+		usuarioServicio.codificarContrasenya(usuario.getPassword(), usuario);
+		usuarioServicio.save(usuario);
+
 		return "redirect:/";
 	}
 }

229d6e9957b4ae1a9294e196f5f48bdefb32acde
Victor <medina.levic24@triana.salesianos.edu>
2024-06-14 11:47:43 +0200

    cambios en el css del boton editar y citas servicios

--- ClinicaMedinaLejeuneVictor/src/main/java/com/salesianostriana/dam/clinicamedinalejeunevictor/servicios/CitaServicio.java
+++ ClinicaMedinaLejeuneVictor/src/main/java/com/salesianostriana/dam/clinicamedinalejeunevictor/servicios/CitaServicio.java
@@ -7,7 +7,16 @@ import com.salesianostriana.dam.clinicamedinalejeunevictor.modelos.CitasPk;
 import com.salesianostriana.dam.clinicamedinalejeunevictor.repositorios.CitaRepositorio;
 import com.salesianostriana.dam.clinicamedinalejeunevictor.servicios.serviciosbase.BaseServiceImpl;
 
+import jakarta.transaction.Transactional;
+
 @Service
 public class CitaServicio extends BaseServiceImpl<Cita, CitasPk, CitaRepositorio> {
 
+	@Transactional
+	@Override
+	public Cita edit(Cita t) {
+
+		return repository.save(t);
+
+	}
 }

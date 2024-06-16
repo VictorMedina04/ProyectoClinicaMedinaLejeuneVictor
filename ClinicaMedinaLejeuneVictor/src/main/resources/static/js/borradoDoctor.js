cf6eabd1701ed55d712d8109b9ab494742b1813c
Victor <victorml2204@gmail.com>
2024-06-16 17:26:58 +0200

    modal para confirmar borrado

---
+++ ClinicaMedinaLejeuneVictor/src/main/resources/static/js/borradoDoctor.js
@@ -0,0 +1,13 @@
+	document.addEventListener('DOMContentLoaded', function () {
+				const deleteButtons = document.querySelectorAll('.delete-button');
+				const deleteModal = document.getElementById('delete-modal');
+				const confirmDeleteBtn = deleteModal.querySelector('.confirm-delete');
+	
+				deleteButtons.forEach(button => {
+					button.addEventListener('click', function () {
+						const doctorId = this.getAttribute('data-doctor-id');
+						const deleteUrl = `/admin/borrarDoctor/${doctorId}`;
+						confirmDeleteBtn.setAttribute('href', deleteUrl);
+					});
+				});
+			});

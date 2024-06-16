8d3057af256ca0b87dd1217f862baae4e444a198
Victor <victorml2204@gmail.com>
2024-06-16 23:56:25 +0200

    cambios en el js

--- ClinicaMedinaLejeuneVictor/src/main/resources/static/js/borradoCita.js
+++ ClinicaMedinaLejeuneVictor/src/main/resources/static/js/borradoCita.js
@@ -1,6 +1,6 @@
- document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function () {
             const deleteButtons = document.querySelectorAll('.delete-button');
-            const deleteModal = new bootstrap.Modal(document.getElementById('delete-modal'));
+			const deleteModal = document.getElementById('delete-modal');
             const confirmDeleteBtn = document.querySelector('.confirm-delete');
             let deleteUrl = '';
 
@@ -9,14 +9,12 @@
                     const idDoctor = button.getAttribute('data-id-doctor');
                     const idCliente = button.getAttribute('data-id-cliente');
                     const fechaInicio = button.getAttribute('data-fecha-inicio');
-					const fechaInicio1 = fechaInicio.replace(' ', 'T');
+					fechaInicio1 = fechaInicio.replace(' ', 'T');
                     deleteUrl = `/admin/borrarCita/${idDoctor}/${idCliente}/${fechaInicio1}`;
                     confirmDeleteBtn.setAttribute('href', deleteUrl);
                     deleteModal.show();
                 });
             });
 
-            confirmDeleteBtn.addEventListener('click', function () {
-                window.location.href = deleteUrl;
-            });
+      
         });
